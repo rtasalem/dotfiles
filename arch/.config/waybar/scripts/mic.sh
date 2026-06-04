69a44644b0432158ce29492a58dd567ab4fea3a1
Rana Salem <ranatasalem@gmail.com>
2026-06-04 21:30:54 +0100

    update waybar sizing

--- arch/.config/waybar/scripts/mic.sh
+++ arch/.config/waybar/scripts/mic.sh
@@ -5,7 +5,7 @@
 # ───────────────────────────────────────────────────────────
 
 if pactl get-source-mute @DEFAULT_SOURCE@ | grep -q 'yes'; then
-    echo "<span foreground='#fab387'>[  ]</span>"
+    echo "<span foreground='#fab387'>[ ]</span>"
 else
-    echo "<span foreground='#56b6c2'>[  ]</span>"
+    echo "<span foreground='#56b6c2'>[]</span>"
 fi
