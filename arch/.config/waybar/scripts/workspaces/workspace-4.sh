c5ea54bfccd14afb8020d1344d9274d48788d115
Rana Salem <ranatasalem@gmail.com>
2026-06-05 16:31:19 +0100

    update waybar

--- arch/.config/waybar/scripts/workspaces/workspace-4.sh
+++ arch/.config/waybar/scripts/workspaces/workspace-4.sh
@@ -2,7 +2,7 @@
 active=$(hyprctl activeworkspace -j | jq '.id')
 
 if [ "$active" -eq 4 ]; then
-  echo "<span foreground='#fab387'>[ 4 ]</span>"
+  echo "<span foreground='#fab387'>[ ● ]</span>"
 else
   echo "<span foreground='#56b6c2'>[ 4 ]</span>"
 fi
